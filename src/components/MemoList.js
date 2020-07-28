f2bb7aecdd16ffa677a222941ff0f71cc2b02f99
坂下桃伽 <sakashitamomoka@sakashitamomotoginoMacBook-Air.local>
2020-07-28 11:48:57 +0900

    Change security rules

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -3,18 +3,18 @@ import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { TouchableHighlight } from 'react-native-gesture-handler';
 
 const dateString = (date) => {
-  // 存在しない場合は空文字列を返すと安全です
+ 
   if (date == null) { return ''; }
-  // firebaseのTimestamp型をDate型に変換する
+  
   const dateObject = date.toDate();
-  // Dateオブジェクトを文字列に変換する
+  
   return dateObject.toISOString().split('T')[0];
 };
 
 
 class MemoList extends React.Component {
   renderMemo({ item }) {
-    console.log(item);
+    
     return (
       <TouchableHighlight onPress={() => { this.props.navigation.navigate('MemoDetail', {memo: item}); }}>
         <View style={styles.memoListItem}>
